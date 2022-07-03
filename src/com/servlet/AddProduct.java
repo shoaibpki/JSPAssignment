da1deee124a9a2052c55ed97c8d42e8519f5e901
Shoaib Raza <shoaib_pki@hotmail.com>
2022-07-04 00:28:23 +0100

    upload JSP Assignment

---
+++ src/com/servlet/AddProduct.java
@@ -0,0 +1,59 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ecommerce.ProductDetail;
+
+/**
+ * Servlet implementation class AddProduct
+ */
+public class AddProduct extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddProduct() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		HttpSession session = request.getSession();
+		
+		ProductDetail p = (ProductDetail) request.getSession().getAttribute("product");
+		
+		session.setAttribute("pid", p.getProductId());
+		session.setAttribute("pname", p.getProductName());
+		session.setAttribute("price", p.getPrice());
+		
+		response.sendRedirect("show.jsp");
+		
+//		RequestDispatcher rd=request.getRequestDispatcher("show.jsp");
+//		rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
